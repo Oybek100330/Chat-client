a73c0725426ff3dcf9def6fcc71387fb22918faf
Oybek <allan.o.azaroff@gmail.com>
2022-02-21 12:08:31 +0500

    check101

--- public/js/utils.js
+++ public/js/utils.js
@@ -3,14 +3,29 @@ const token = window.localStorage.getItem('token')
 
 async function request (route, method, body) {
 	
+	let headers = {
+		token: token
+	}
+
+	if(!(body instanceof FormData)) {
+		headers['Content-Type'] = 'application/json'
+	}
+
 	let response = await fetch(hostname + route, {
 		method,
-		headers: {
-			'Content-Type': 'application/json',
-			token: token
-		},
-		body: body ? JSON.stringify(body) : null
+		headers,
+		body: (body instanceof FormData) ? body : JSON.stringify(body)
 	})
+
+
+	// let response = await fetch(hostname + route, {
+	// 	method,
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 		token: token
+	// 	},
+	// 	body: body ? JSON.stringify(body) : null
+	// })
 
 	if([400, 404, 413, 415].includes(response.status)) {
 		response = await response.json()
