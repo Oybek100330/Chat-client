4dff1d78dd4873842fd1a76a8539d59d287d330e
Oybek <allan.o.azaroff@gmail.com>
2022-02-21 12:01:14 +0500

    check100

--- public/js/register.js
+++ public/js/register.js
@@ -9,7 +9,7 @@ registerForm.onsubmit = async event => {
 
 		formData.append('username', usernameInput.value)
 		formData.append('password', passwordInput.value)
-		formData.append('file', uploadInput.files[0])
+		formData.append('avatar', uploadInput.files[0])
 
 		let response = await request('/auth/register', 'POST', formData)
 		// console.log(response);
